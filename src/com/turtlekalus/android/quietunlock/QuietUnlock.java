04db46e37e02ff5b291b3b769d6b7c81c0182221
Turtle Kalus <tkalus@server.fake>
2013-01-15 18:08:41 -0800

    Attempt to fix Dialog not showing up on certain devices

--- src/com/turtlekalus/android/quietunlock/QuietUnlock.java
+++ src/com/turtlekalus/android/quietunlock/QuietUnlock.java
@@ -23,6 +23,9 @@ import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Build;
+import android.os.Build.VERSION;
+import android.os.Build.VERSION_CODES;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -30,6 +33,8 @@ import android.os.RemoteException;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.Window;
+import android.view.WindowManager;
+import android.view.WindowManager.LayoutParams;
 import android.widget.Button;
 import android.widget.Toast;
 import java.util.ArrayList;
@@ -84,10 +89,6 @@ public class QuietUnlock extends Activity
         doCancel();
     }
 
-    private void showDialog() {
-        QuietUnlockDialog.newInstance(R.string.app_name).show(getFragmentManager(), "dialog");
-    }
-
     protected void doUpdateSilentVibe(int which) {
         switch(which) {
             case SELECT_SILENT:
@@ -135,60 +136,66 @@ public class QuietUnlock extends Activity
     }
 
     // Main body of the UI.
-    public static class QuietUnlockDialog extends DialogFragment {
-        public static QuietUnlockDialog newInstance(int title) {
-            QuietUnlockDialog dialog = new QuietUnlockDialog();
-            Bundle args = new Bundle();
-            args.putInt("title", title);
-            dialog.setArguments(args);
+    private void showDialog() {
+        CharSequence[] items = {"",""};
+        items[SELECT_SILENT] = this.getString(R.string.select_silent);
+        items[SELECT_VIBRATE] = this.getString(R.string.select_vibrate);
+        int checkedItem = isSilent ? SELECT_SILENT : SELECT_VIBRATE;
+        AlertDialog.Builder alertDialogBuilder;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+            alertDialogBuilder = new AlertDialog.Builder(this, AlertDialog.THEME_DEVICE_DEFAULT_DARK);
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            alertDialogBuilder = new AlertDialog.Builder(this, AlertDialog.THEME_HOLO_DARK);
+        } else {
+            alertDialogBuilder = new AlertDialog.Builder(this);
+        }
+        AlertDialog alertDialog = alertDialogBuilder
+            // Dialog Title "Quiet Unlock"; set in the res/xml
+            .setTitle(R.string.app_name)
+            // "Silent" / "Vibrate" Radio Buttons
+            .setSingleChoiceItems(items, checkedItem,
+                    new DialogInterface.OnClickListener() {
+                        @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    QuietUnlock.this.doUpdateSilentVibe(which);
+                }})
+            // "OK" Button
+            .setPositiveButton(R.string.alert_dialog_ok,
+                    new DialogInterface.OnClickListener() {
+                        @Override
+                public void onClick(DialogInterface dialog, int whichButton) {
+                    QuietUnlock.this.doOK();
+                }})
+            // "Cancel" Button
+            .setNegativeButton(R.string.alert_dialog_cancel,
+                    new DialogInterface.OnClickListener() {
+                        @Override
+                public void onClick(DialogInterface dialog, int whichButton) {
+                    QuietUnlock.this.doCancel();
+                }})
+            // Home/Back/(Any) Key Listener/Handler
+            .setOnKeyListener(
+                    new DialogInterface.OnKeyListener() {
+                        @Override
+                public boolean onKey(DialogInterface dialog,  int keyCode, KeyEvent event) {
+                    if (KeyEvent.KEYCODE_MENU != keyCode) {
+                        QuietUnlock.this.doCancel();
+                    }
+                    return true;
+                }})
+            .setIcon(R.drawable.noog)
             // Clicks/Touches outside the Dialog are non-reactive.
             // User must instead use the dialog buttons or Home or
             // Back keys
-            dialog.setCancelable(false);
-            return dialog;
-        }
-        @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-            int title = getArguments().getInt("title");
-            // Build up Radio Button list.
-            CharSequence[] items = {"",""};
-            items[SELECT_SILENT] = this.getString(R.string.silent);
-            items[SELECT_VIBRATE] = this.getString(R.string.vibrate);
-            int checkedItem = isSilent ? SELECT_SILENT : SELECT_VIBRATE;
-            AlertDialog dialog = new AlertDialog.Builder(getActivity())
-                // Dialog Title "Quiet Unlock"; set in the res/xml
-                .setTitle(title)
-                // "Silent" / "Vibrate" Radio Buttons
-                .setSingleChoiceItems(items, checkedItem,
-                        new DialogInterface.OnClickListener() {
-                            @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        ((QuietUnlock)getActivity()).doUpdateSilentVibe(which);
-                    }})
-                // "OK" Button
-                .setPositiveButton(R.string.alert_dialog_ok,
-                        new DialogInterface.OnClickListener() {
-                            @Override
-                    public void onClick(DialogInterface dialog, int whichButton) {
-                        ((QuietUnlock)getActivity()).doOK();
-                    }})
-                // "Cancel" Button
-                .setNegativeButton(R.string.alert_dialog_cancel,
-                        new DialogInterface.OnClickListener() {
-                            @Override
-                    public void onClick(DialogInterface dialog, int whichButton) {
-                        ((QuietUnlock)getActivity()).doCancel();
-                    }})
-                // Home/Back/(Any) Key Listener/Handler
-                .setOnKeyListener(
-                        new DialogInterface.OnKeyListener() {
-                            @Override
-                    public boolean onKey(DialogInterface dialog,  int keyCode, KeyEvent event) {
-                        ((QuietUnlock)getActivity()).doCancel();
-                        return true;
-                    }})
-                .create();
-                return dialog;
-        }
+            .setCancelable(false)
+            .show();
+
+            // Fix candidate for where dialog doesn't show on some Phones.
+            // from http://stackoverflow.com/questions/2306503/how-to-make-an-alert-dialog-fill-90-of-screen-size/6631310#6631310
+            WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
+            lp.copyFrom(alertDialog.getWindow().getAttributes());
+            lp.width = WindowManager.LayoutParams.FILL_PARENT;
+            lp.height = WindowManager.LayoutParams.FILL_PARENT;
+            alertDialog.getWindow().setAttributes(lp);
     }
 }
